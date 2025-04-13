c3c47c6fb9374a4af9d9c0677cfcd53ae0ec5cb8
Thibault <thibault.borde@etu.emse.fr>
2025-04-13 16:48:27 +0200

    added Hook on custom state property

--- vizrevent/src/DS/DSController.js
+++ vizrevent/src/DS/DSController.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import DSView from './DSView'
+import { useStoreSelector } from '../Store/VizreventStore';
 
 
 
 const DSController = () => {
+    const { state, dispatch } = useStoreSelector(state => ({
+        vizParam: state.vizParam,
+        dataset: state.dataset,
+    }));
 
-    
+  // console.log("DSController useStoreSelector Output:");
+  // console.log(useStoreSelector(state => ({
+  //     vizParam: state.vizParam,
+  //     dataset: state.dataset,
+  // })));
+
+
     return (
         <>
             <DSView />
+            <h1>DSControllerPseudoState:{JSON.stringify(state, null, 2)}</h1>
         </>
     )
 }
--- vizrevent/src/Store/VizreventStore.js
+++ vizrevent/src/Store/VizreventStore.js
@@ -1,4 +1,4 @@
-import React, { useReducer, createContext, useContext } from 'react';
+import React, { useReducer, createContext, useContext, useMemo } from 'react';
 import reducer, { initialState } from './VizreventReducer';
 import {
   setRecSettings,
@@ -10,30 +10,42 @@ import {
 // Create a context for the store
 const StoreContext = createContext();
 
-// StoreProvider component
 export const StoreProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const contextValue = useMemo(() => ({
+    state,
+    dispatchMethods: {
+      setRecSettings: (recSettings) => dispatch(setRecSettings(recSettings)),
+      setVizParam: (vizParam) => dispatch(setVizParam(vizParam)),
+      setDataset: (dataset) => dispatch(setDataset(dataset)),
+      setInputViz: (inputViz) => dispatch(setInputViz(inputViz)),
+    },
+  }), [state]);
+
   return (
-    <StoreContext.Provider
-      value={{
-        state,
-        setRecSettings: (recSettings) => dispatch(setRecSettings(recSettings)),
-        setVizParam: (vizParam) => dispatch(setVizParam(vizParam)),
-        setDataset: (dataset) => dispatch(setDataset(dataset)),
-        setInputViz: (inputViz) => dispatch(setInputViz(inputViz)),
-      }}
-    >
+    <StoreContext.Provider value={contextValue}>
       {children}
     </StoreContext.Provider>
   );
 };
 
-// Custom hook to use the store
-export const useStore = () => {
+
+// Custom hook to use the store with possible selection of relevant properties and dispatch methods
+export const useStoreSelector = (stateSelector = state => state, dispatchSelector = dispatchMethods => dispatchMethods)=> {
   const context = useContext(StoreContext);
   if (context === undefined) {
-    throw new Error('useStore must be used within a StoreProvider');
+    throw new Error('useStoreSelector must be used within a StoreProvider');
   }
-  return context;
+
+  const { state, dispatchMethods } = context;
+  const selectedState = stateSelector(state);
+  const selectedDispatchMethods = dispatchSelector(dispatchMethods);
+ // console.log('Dispatch Methods:', dispatchMethods);
+ // console.log('Selected Dispatch Methods:', selectedDispatchMethods);
+
+  return {
+    state:selectedState,
+    ...selectedDispatchMethods,
+  };
 };
