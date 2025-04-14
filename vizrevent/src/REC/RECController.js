76c0b2830904c6a3a8aac1d03a9f3181371c63fc
Thibault <thibault.borde@etu.emse.fr>
2025-04-14 16:55:04 +0200

    added open/close RECPanel

--- vizrevent/src/REC/RECController.js
+++ vizrevent/src/REC/RECController.js
@@ -1,12 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import RECView from './RECView'
+import { useStoreSelector } from '../Store/VizreventStore';
 
 
 
 const RECController = () => {
+
+    //connecting to store
+    const { state, dispatch } = useStoreSelector(state => ({
+        vizParam: state.vizParam,
+        recSettings: state.recSettings,
+        dataset: state.dataset,
+    }));
+
+    //Creating local state 
+    const [isOpened, setIsOpened] = useState(false);
+    const [recList, setRecList] = useState([
+        { id: 1 },
+        { id: 2 },
+        { id: 3 }
+    ]);
+
+    //console.log("RECController useStoreSelector Output:");
+    //console.log(dispatch);
+
+
+
+    //function that actually computes the recommendation
+    const recCompute = (vizParam,recSettings,dataset) => {
+        const recList = [
+            { id: 1 },
+            { id: 2 },
+            { id: 3 }
+        ];
+
+        setRecList(recList);
+    }
+
+    ///////Event Handlers
+
+    //"Opens" and display RECView on expanding button
+    const handlePanelOpener = (currentIsOpened) => {
+        setIsOpened(!(currentIsOpened));
+    };
+
+
     return (
         <>
-            <RECView />
+            <RECView isOpened={isOpened}
+                onPanelOpenerClick={handlePanelOpener}
+                recList={recList} />
+            <h1>RECControllerPseudoState:{JSON.stringify(state, null, 2)}</h1>
+            <h1>local state:{JSON.stringify(isOpened, null, 2)}</h1>
         </>
     )
 }
--- vizrevent/src/REC/RECView.js
+++ vizrevent/src/REC/RECView.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React from 'react';
 
-const RECView = () => {
-    return (
-      <div style={{ backgroundColor: 'blue' }}>
-        RECView
-      </div>
-    );
+const RECView = (props) => {
+
+  const { isOpened, onPanelOpenerClick, recList } = props;
+
+
+  const handlePanelExpandClick = () => {
+    // List of data field selected to be passed to RECController
+    onPanelOpenerClick(isOpened);
   };
-  
 
-export default RECView
+
+  //display one recommendation item
+  const displayRECItem = (recItem) => {
+
+
+    return <button key={recItem.id}>recommendation n°{recItem.id}</button>;
+
+
+  }
+
+  return (
+    <div style={{ backgroundColor: 'blue' }}>
+      {isOpened && (
+        <div className='RECPanel' style={{ backgroundColor: 'blue' }}>
+          <div className='RECVizList'>
+            {recList.map(recItem => displayRECItem(recItem))}
+          </div>
+        </div>
+      )}
+      <h1>RECView</h1>
+
+      <button onClick={handlePanelExpandClick}>
+        {isOpened ? "Close Panel" : " Open panel"}
+      </button>
+
+    </div>
+  );
+};
+export default RECView;
