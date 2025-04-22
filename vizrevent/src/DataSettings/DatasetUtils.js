811beeea8b58d1bc7dbd0ddd651e0453fcf95546
Thibault <thibault.borde@etu.emse.fr>
2025-04-22 16:37:22 +0200

    added datasetList fetch method

--- vizrevent/src/DataSettings/DatasetUtils.js
+++ vizrevent/src/DataSettings/DatasetUtils.js
@@ -12,3 +12,18 @@ export const DatasetFetcher = async (params = {},serverUrl = 'http://localhost:5
         throw error;
     }
 };
+
+export const DatasetListFetcher = async (serverUrl = 'http://localhost:5000/api/datasetList') =>{
+    try {
+        const response = await fetch(`${serverUrl}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const jsonData = await response.json();
+        return jsonData;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+
+}
