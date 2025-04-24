1f91d7013e1dab74637ce0d5d7af0c605f3b4576
Thibault <thibault.borde@etu.emse.fr>
2025-04-24 10:47:09 +0200

    added data fetch

--- vizrevent/src/Viz/Viz.js
+++ vizrevent/src/Viz/Viz.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useStoreSelector } from '../Store/VizreventStore';
 import { VegaLite } from 'react-vega'
+import { DatasetFetcher } from '../DataSettings/DatasetUtils';
 
 const Viz = (props) => {
     const { vizQuery } = props;
@@ -11,12 +12,30 @@ const Viz = (props) => {
         datasetId: state.datasetId,
     }));
 
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await DatasetFetcher(state.datasetId);
+                setData(result);
+            } catch (err) {
+                console.error('Error fetching dataset:', err);
+            }
+        };
+
+        if (state.datasetId) {
+            fetchData();
+        }
+    }, []);
+
+    console.log("Viz/data:", data);
     //Ensuring data format for Vega - Lite, Draco and Vega-lite requires different data format so we convert it here to fit Vega-lite
     const spec = {
 
         schema: "https://vega.github.io/schema/vega-lite/v3.json",
         data: {
-            values: state.datasetId
+            values: data
         },
         ...vizQuery,
 
@@ -31,7 +50,7 @@ const Viz = (props) => {
     return (
         <>
             {vizQuery && vizQuery !== null ? (
-                <VegaLite data={state.datasetId} spec={spec} />
+                <VegaLite data={data} spec={spec} />
             ) : ( //placeholder for empty viz
                 <div style={{ border: '1px solid black', width: '200px', height: '100px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                     <span>No Visualization Available</span>
