874e32b135fe93b122f092075dc88fed2a053764
Thibault <thibault.borde@etu.emse.fr>
2025-05-03 15:43:47 +0200

    update fieldList fetch

--- vizrevent/src/DataSettings/DatasetUtils.js
+++ vizrevent/src/DataSettings/DatasetUtils.js
@@ -1,5 +1,3 @@
-import Draco from "draco-vis";
-
 export const DatasetFetcher = async (params = {}, serverUrl = 'http://localhost:5000/api/dataset') => {
     try {
 
@@ -7,7 +5,7 @@ export const DatasetFetcher = async (params = {}, serverUrl = 'http://localhost:
 
         const response = await fetch(`${serverUrl}?${queryString}`);
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
         const jsonData = await response.json();
         return jsonData;
@@ -21,7 +19,7 @@ export const DatasetListFetcher = async (serverUrl = 'http://localhost:5000/api/
     try {
         const response = await fetch(`${serverUrl}`);
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
         const jsonData = await response.json();
         return jsonData;
@@ -31,25 +29,32 @@ export const DatasetListFetcher = async (serverUrl = 'http://localhost:5000/api/
     }
 
 }
-
-export const DatafieldsList = async (datasetId = {}, serverUrl = 'http://localhost:5000/api/dataset') => {
-
-    // Call DracoRecProcess with the dataset
-
-    try {
-        const data = await DatasetFetcher(datasetId)
-
-        //Here we only use draco-vis to read the data fields
-        const draco = new Draco();
-        draco.prepareData(data);
-        console.log("DataFields", draco.getSchema());
-        return draco.getSchema().stats; //we only want the stats property from the getSchema output
+export const DatafieldsList = async (dataset_id = null, serverUrl = 'http://localhost:5000/api/datafields') => {
+    
+    if (dataset_id) {
+        try {
 
 
-    } catch (error) {
-        console.error('Error in getting data fields:', error);
+            // Construct the URL with the dataset_id if provided
+            const url = dataset_id ? `${serverUrl}?dataset_id=${dataset_id}` : serverUrl;
 
+            // Send a GET request to the server
+            const response = await fetch(url);
+
+            // Check if the response is ok (status code 200-299)
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            // Parse the JSON response
+            const data = await response.json();
+            // Return the list of data fields
+            return data;
+
+        } catch (error) {
+            // Handle any errors that occur during the fetch operation
+            console.error('Error fetching data fields:', error);
+            throw error;
+        }
     }
-
-
 };
