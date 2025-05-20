728390850d780ec564d07ee074e0161b3cc25ff7
Thibault <thibault.borde@etu.emse.fr>
2025-05-20 17:25:43 +0200

    reshaped datafieldselection into table

--- vizrevent/src/DataSettings/DataFieldSelection/DataFieldSelection.js
+++ vizrevent/src/DataSettings/DataFieldSelection/DataFieldSelection.js
@@ -1,36 +1,41 @@
-import React from 'react'
-import './DataFieldSelection.scss'
-
-
+import React from 'react';
+import './DataFieldSelection.scss';
 
 const DataFieldSelection = ({ dataFields, selectedFields, handleCheckboxChange }) => {
-
-
     return (
         <>
             {dataFields ? (
                 dataFields.length > 0 ? (
-                    dataFields.map((field) => (
-                        <div
-                            key={field.name}
-                            className="field-item"
-                            title={`Field: ${field.name}
-                                    Type: ${field.type}
-                                    Frequency: ${field.freq}
-                                    Entropy: ${field.entropy}
-                                    Unique Values: ${field.unique}`}
-                        >
-                            <label>
-                                <input
-                                    type="checkbox"
-                                    checked={selectedFields.includes(field)}
-                                    onChange={() => handleCheckboxChange(field)}
-                                />
-                                <b>{field.type} - </b>
-                                {field.name}
-                            </label>
-                        </div>
-                    ))
+                    <table className="data-field-table">
+                        <thead>
+                            <tr>
+                                <th>Select</th>
+                                <th>Type</th>
+                                <th>Name</th>
+                                <th>Frequency</th>
+                                <th>Entropy</th>
+                                <th>Unique Values</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {dataFields.map((field) => (
+                                <tr key={field.name} title={`Field: ${field.name}\nType: ${field.type}\nFrequency: ${field.freq}\nEntropy: ${field.entropy}\nUnique Values: ${field.unique}`}>
+                                    <td>
+                                        <input
+                                            type="checkbox"
+                                            checked={selectedFields.includes(field)}
+                                            onChange={() => handleCheckboxChange(field)}
+                                        />
+                                    </td>
+                                    <td><b>{field.type}</b></td>
+                                    <td>{field.name}</td>
+                                    <td>{field.freq}</td>
+                                    <td>{field.entropy}</td>
+                                    <td>{field.unique}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 ) : (
                     <p className="fallback-message">Loading data fields...</p>
                 )
@@ -40,6 +45,5 @@ const DataFieldSelection = ({ dataFields, selectedFields, handleCheckboxChange }
         </>
     );
 };
-
 
 export default DataFieldSelection;
