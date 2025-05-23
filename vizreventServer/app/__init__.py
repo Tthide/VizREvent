10822527c3d54d18b34b597e86bf8be92a5c0513
Thibault <thibault.borde@etu.emse.fr>
2025-05-23 12:24:56 +0200

    exe prod prepare

--- vizreventServer/app/__init__.py
+++ vizreventServer/app/__init__.py
@@ -1,15 +1,38 @@
-from flask import Flask
+import sys
+import os
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 
 def create_app():
-    app = Flask(__name__)
-    CORS(app)  # Enable CORS for all routes
+    if getattr(sys, 'frozen', False):
+        # Running inside PyInstaller bundle
+        base_path = sys._MEIPASS
+    else:
+        # Running normally
+        base_path = os.path.abspath(os.path.dirname(__file__))
+
+    static_folder_path = os.path.join(base_path, "frontend_build")
+    
+    app = Flask(__name__, static_folder=static_folder_path, static_url_path="")
+    CORS(app)
 
     # Register blueprints
     from .routes.data_routes import data_bp
     app.register_blueprint(data_bp)
     from .routes.draco_routes import draco_bp
     app.register_blueprint(draco_bp)
+    
+    
+    @app.route('/')
+    def serve_react():
+        return send_from_directory(app.static_folder, 'index.html')
 
+    @app.route('/<path:path>')
+    def serve_static(path):
+        file_path = os.path.join(app.static_folder, path)
+        if os.path.exists(file_path):
+            return send_from_directory(app.static_folder, path)
+        else:
+            return send_from_directory(app.static_folder, 'index.html')
 
     return app
--- vizreventServer/run.py
+++ vizreventServer/run.py
@@ -1,8 +1,11 @@
 from app import create_app
 import os
+import webbrowser
+from threading import Timer
 
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or default 5000 locally
+    port = int(os.environ.get("PORT", 5000))
+    Timer(1.0, lambda: webbrowser.open(f"http://127.0.0.1:{port}")).start()
     app.run(host="0.0.0.0", port=port)
