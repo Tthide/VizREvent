06e20bffddd58a287684af82be4833f4a3e61280
Thibault <thibault.borde@etu.emse.fr>
2025-05-26 10:44:37 +0200

    created dev_mode to flask server

--- vizreventServer/run.py
+++ vizreventServer/run.py
@@ -5,7 +5,13 @@ from threading import Timer
 
 app = create_app()
 
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    Timer(1.0, lambda: webbrowser.open(f"http://127.0.0.1:{port}")).start()
+    
+    # Only open browser if DEV_MODE is not set to true
+    dev_mode = os.environ.get("DEV_MODE", "false").lower() == "true"
+    if not dev_mode:
+        Timer(1.0, lambda: webbrowser.open(f"http://127.0.0.1:{port}")).start()
+    
     app.run(host="0.0.0.0", port=port)
